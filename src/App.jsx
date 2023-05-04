cf6835061db278ba5817607869d08ee618322ee5
DESKTOP-NOMKETR\adnan <11adnan@live.com>
2023-05-04 12:43:52 +0600

    Birthday project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
-import Person from "./Person";
-
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 const App = () => {
+  const [people, setPeople] = useState(data);
+
   return (
-    <>
-      <h2>Birthday Reminder - Starter</h2>
-      <Person />
-    </>
+    <main>
+      <section className="container">
+        <h2>Birthday Reminder - Starter</h2>
+        <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
   );
 };
 export default App;
--- src/Person.jsx
+++ src/Person.jsx
@@ -1,25 +1,13 @@
-import data from "./data";
 
-const Person = () => {
-  console.log(data);
+const Person = ({ image, name, age }) => {
   return (
-    <>
-      <h3>{data.length} birthday today</h3>
-      {data.map((person) => {
-        return (
-          <div key={person.id}>
-            <img
-              style={{ height: "150px", width: "150px" }}
-              src={person.image}
-              alt={person.name}
-              className="img"
-            />
-            <h5>{person.name}</h5>
-            <p>{person.age} years old</p>
-          </div>
-        );
-      })}
-    </>
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
   );
 };
 export default Person;
